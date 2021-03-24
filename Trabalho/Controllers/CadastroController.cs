faf070c968b47cee366ae23a9b8720be25f78f91
guilehrme <guilherme.lehr@gmail.com>
2021-03-24 12:13:38 -0300

    Adição da janela de Idiomas

--- Trabalho/Controllers/CadastroController.cs
+++ Trabalho/Controllers/CadastroController.cs
@@ -31,12 +31,44 @@ namespace Trabalho.Controllers
         {
             return View();
         }
+        public IActionResult CadastroIdiomas()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        private void ValidaDados(CurriculoViewModel curriculo, string operacao)
+        {
+            /*CurriculoDAO dao = new CurriculoDAO();
+            if (operacao == "I" && dao.Consulta(curriculo.Id) != null)
+                ModelState.AddModelError("Id", "Código já está em uso.");
+            if (operacao == "A" && dao.Consulta(curriculo.Id) == null)
+                ModelState.AddModelError("Id", "Aluno não existe.");
+            if (curriculo.Id <= 0)
+                ModelState.AddModelError("Id", "Id inválido!");
+
+            if (string.IsNullOrEmpty(curriculo.Nome))
+                ModelState.AddModelError("Nome", "Preencha o nome.");
+            if (curriculo.Mensalidade < 0)
+                ModelState.AddModelError("Mensalidade", "Campo obrigatório.");
+            if (curriculo.CidadeId <= 0)
+                ModelState.AddModelError("CidadeId", "Informe o código da cidade.");
+            if (curriculo.DataNascimento > DateTime.Now)
+                ModelState.AddModelError("DataNascimento", "Data inválida!");
+        }
+
+        private void ValidaDados(CurriculoViewModel curriculo, string operacao)
+        {
+            CurriculoDAO dao = new CurriculoDAO();
+            if (ModelState.IsValid )
+                ModelState.AddModelError("Id", "Código já está em uso.");*/
+        }
+
     }
 }
 
--- Trabalho/Models/CurriculoViewModel.cs
+++ Trabalho/Models/CurriculoViewModel.cs
@@ -8,5 +8,31 @@ namespace Trabalho.Models
     public class CurriculoViewModel
     {
         public int id { get; set; }
+        public int cpf { get; set; }
+        public string nome { get; set; }
+        public int celular { get; set; }
+        public string email { get; set; }
+        public int cep { get; set; }
+        public string rua { get; set; }
+        public int numeroEnd { get; set; }
+        public string bairro { get; set; }
+        public string complementoEnd { get; set; }
+        public string estado { get; set; }
+        public string formacao { get; set; }
+        public string curso1 { get; set; }
+        public string curso2 { get; set; }
+        public string curso3 { get; set; }
+        public string curso4 { get; set; }
+        public string curso5 { get; set; }
+        public string experiencia1 { get; set; }
+        public string experiencia1Desde { get; set; }
+        public string experiencia1Ate { get; set; }
+        public string experiencia2 { get; set; }
+        public string experiencia2Desde { get; set; }
+        public string experiencia2Ate { get; set; }
+        public string experiencia3 { get; set; }
+        public string experiencia3Desde { get; set; }
+        public string experiencia3Ate { get; set; }
+        public string cargoPretendido { get; set; }
     }
 }
