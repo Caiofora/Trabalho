b0727241886dd6097700529c9b0916a72a588fef
guilehrme <guilherme.lehr@gmail.com>
2021-04-01 13:03:51 -0300

    Adição do SweetAlert

--- Trabalho/wwwroot/js/site.js
+++ Trabalho/wwwroot/js/site.js
@@ -22,6 +22,60 @@
 
 }
 
-function ListaCursos(CurriculoViewModel Select) {
+function SalvarCurriculo(id) {
+    swal({
+        position: 'center',
+        type: 'success',
+        title: 'Curriculo Salvo!',
+        showConfirmButton: false,
+        timer: 2000
+    },
+        function () {
+            location.href = '/Cadastro/Salvar4';
+        });
+};
 
+function FinalRedirect() {
+    location.href = '/Home/Index';
 }
+
+
+function apagarCurriculo(id) {
+
+    swal({
+        title: "Tem certeza?",
+        text: "O Curriculo será apagado para sempre!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonClass: "btn-danger",
+        cancelButtonClass: "btn-info",
+        confirmButtonText: "Sim",
+        cancelButtonText: "Não!",
+        closeOnConfirm: false
+    },
+        function aparecerSimbolo() {
+            swal({
+                position: 'center',
+                type: 'success',
+                title: 'Curriculo excluído!',
+                showConfirmButton: false,
+                timer: 1500
+            },
+                function () {
+                    location.href = '/Home/ExcluirCurriculo?id=' + id;
+                });
+        });
+};
+
+/*function aparecerSimbolo() {
+    swal({
+        position: 'center',
+        icon: 'success',
+        title: 'Curriculo excluído!',
+        showConfirmButton: false,
+        timer: 1500
+    },
+    function () {
+        location.href = '/Home/ExcluirCurriculo?id=' + id;
+    });
+};*/
